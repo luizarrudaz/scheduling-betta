1ca999cf7f1a0f0acb3dfcaf80d65341aff18c50
luiz.arruda <luiz.arruda113@gmail.com>
2025-02-17 10:28:32 -0300

    ISchedule Repository & Service

---
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Interfaces/IScheduleService.cs
@@ -0,0 +1,20 @@
+﻿using SchedulingBetta.API.DTOs;
+using SchedulingBetta.API.Entitites;
+
+namespace SchedulingBetta.API.Interfaces;
+
+public interface IScheduleService
+{
+    Task<IEnumerable<Schedule>> GetAllSchedules();
+    Task<Schedule> GetScheduleById(int id);
+    Task<int> CreateSchedule(Schedule schedule);
+    Task<bool> UpdateSchedule(int id, UpdateScheduleDTO dto);
+    Task<bool> CancelSchedule(int id);
+    Task<IEnumerable<Schedule>> GetSchedulesByDate(DateTime date);
+    Task<IEnumerable<Schedule>> GetSchedulesByService(string service);
+    Task<IEnumerable<Schedule>> GetSchedulesByUser(int userId);
+    Task<int> AddInterestedUser(int scheduleId, int userId);
+    Task<IEnumerable<InterestedUser>> GetInterestedUsers(int scheduleId);
+    Task<bool> RemoveInterestedUser(int id);
+    Task<IEnumerable<ScheduleLog>> GetLogs(int scheduleId);
+}
