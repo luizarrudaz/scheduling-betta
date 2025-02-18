6d6b64d83a41eba76294615d25e4d6963ace8115
luiz.arruda <luiz.arruda113@gmail.com>
2025-02-18 11:39:01 -0300

    Authentication JWT & LdapAuth Service added

---
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Authentication/JwtService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using DotNetEnv;
+
+namespace SchedulingBetta.API.Authentication
+{
+    public class JwtService
+    {
+        private readonly string _secretKey;
+        private readonly string _issuer;
+
+        public JwtService()
+        {
+            Env.Load();
+
+            _secretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY") ?? 
+                throw new ArgumentNullException(nameof(_secretKey));
+
+            _issuer = Environment.GetEnvironmentVariable("JWT_ISSUER") ?? 
+                throw new ArgumentNullException(nameof(_issuer));
+        }
+
+        public string GenerateJwtToken(string username, List<string> groups)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim("role", groups.Contains("Admins") ? "Admin" : "User")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _issuer,
+                audience: _issuer,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(10),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Authentication/LdapAuthService.cs
@@ -0,0 +1,54 @@
+﻿using DotNetEnv;
+using System.DirectoryServices.AccountManagement;
+
+namespace SchedulingBetta.API.Authentication;
+
+public class LdapAuthService
+{
+    private readonly string _domain;
+
+    public LdapAuthService()
+    {
+        Env.Load();
+
+        _domain = Environment.GetEnvironmentVariable("DOMAIN") ??
+            throw new ArgumentNullException(nameof(_domain));
+
+    }
+
+    public bool AuthenticateUser(string username, string password)
+    {
+        using (var context = new PrincipalContext(ContextType.Domain, _domain))
+        {
+            try
+            {
+                bool isValid = context.ValidateCredentials(username, password);
+                return isValid;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}"); // log
+                return false;
+            }
+        }
+
+    }
+
+    public List<string> GetUserGroups(string username)
+    {
+        List<string> groups = new List<string>();
+        using (var context = new PrincipalContext(ContextType.Domain, _domain))
+        using (var user = UserPrincipal.FindByIdentity(context, username))
+        {
+            if (user != null)
+            {
+                var groupPrincipal = user.GetGroups();
+                foreach (var group in groupPrincipal)
+                {
+                    groups.Add(group.Name);
+                }
+            }
+        }
+        return groups;
+    }
+}
