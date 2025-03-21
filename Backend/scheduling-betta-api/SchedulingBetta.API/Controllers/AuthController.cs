12d9cc7ce377a96ae12fea6854dc118be37aef99
luiz.arruda <luiz.arruda113@gmail.com>
2025-03-21 17:01:38 -0300

    feat(auth): implements authentication api

---
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using SchedulingBetta.API.Authentication;
+using LoginRequest = SchedulingBetta.API.Authentication.LoginRequest;
+
+namespace SchedulingBetta.API.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly LdapAuthService _ldapAuth;
+    private readonly JwtService _jwtService;
+
+    public AuthController()
+    {
+        _ldapAuth = new LdapAuthService();
+        _jwtService = new JwtService();
+    }
+
+    [HttpPost("Login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        try
+        {
+            // AD Authentication
+            if(!_ldapAuth.AuthenticateUser(request.Username, request.Password))
+            {
+                return Unauthorized("Invalid Credentials");
+            }
+
+            var groups = _ldapAuth.GetUserGroups(request.Username);
+
+            var token = _jwtService.GenerateJwtToken(request.Username, groups);
+
+            return Ok(new { Token = token });
+        } 
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal error: {ex.Message}");
+        }
+    }
+}
