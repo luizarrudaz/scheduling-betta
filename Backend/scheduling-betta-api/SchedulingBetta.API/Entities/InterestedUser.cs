145242642f7ef45552bd7e0cca28a9334ae1ab7e
luiz.arruda <luiz.arruda113@gmail.com>
2025-03-31 13:10:54 -0300

    refactor(entities): update InterestedUser model structure

--- Backend/scheduling-betta-api/SchedulingBetta.API/Entities/InterestedUser.cs
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Entities/InterestedUser.cs
@@ -1,8 +1,22 @@
-﻿namespace SchedulingBetta.API.Entitites;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchedulingBetta.API.Entities;
 
 public class InterestedUser
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    public int ScheduleId { get; set; }
-    public int UserId { get; set; }
+
+    [Required]
+    [ForeignKey("Event")]
+    public int EventId { get; set; }
+    public Event Event { get; set; }
+
+    [Required]
+    public string UserId { get; set; } // AD user identifier
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
