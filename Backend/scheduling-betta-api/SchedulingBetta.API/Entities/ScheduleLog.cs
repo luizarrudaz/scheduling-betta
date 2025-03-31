0251fe7abe0097e2de80ff13d4d7f9ec2fb07bdc
luiz.arruda <luiz.arruda113@gmail.com>
2025-03-31 13:10:29 -0300

    chore(database): remove legacy schema files and entities

--- Backend/scheduling-betta-api/SchedulingBetta.API/Entities/ScheduleLog.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchedulingBetta.API.Entitites;
-
-public class ScheduleLog
-{
-    public int Id { get; set; }
-    public int ScheduleId { get; set; }
-    public string Action { get; set; } 
-    public DateTime Timestamp { get; set; }
-}
