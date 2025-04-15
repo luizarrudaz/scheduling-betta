5b68dff2b69006e881e1bbc636ec3b3ed81fd9e7
luiz.arruda <luiz.arruda113@gmail.com>
2025-04-15 16:03:52 -0300

    feat(AuthorizeBearer): add AuthorizeBearer

--- Backend/scheduling-betta-api/SchedulingBetta.API/Program.cs
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Program.cs
@@ -2,47 +2,51 @@ using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Env.Load(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
+Env.Load();
 
 builder.Services.AddDbContext<SchedulingDbContext>(options =>
-{
-    options.UseNpgsql(Env.GetString("DB_CONNECTION_STRING"));
-
-    if (builder.Environment.IsDevelopment())
-    {
-        options.EnableSensitiveDataLogging()
-               .LogTo(Console.WriteLine, LogLevel.Information);
-    }
-});
+    options.UseNpgsql(Env.GetString("DB_CONNECTION_STRING")));
 
 builder.Services.AddCors(options =>
-{
     options.AddPolicy("AllowFrontend", policy =>
-    {
         policy.WithOrigins("http://localhost:5173")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
-    });
-});
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options => {
     options.RequireHttpsMetadata = !builder.Environment.IsDevelopment();
-    options.SaveToken = true;
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
@@ -56,10 +60,7 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
-builder.Configuration.AddEnvironmentVariables();
-
 builder.Services.AddScoped<JwtService>();
-
 builder.Services.AddScoped<LdapAuthService>();
 
 var app = builder.Build();
