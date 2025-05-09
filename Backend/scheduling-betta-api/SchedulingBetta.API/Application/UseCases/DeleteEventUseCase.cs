48d631d44423f136bb5f730ce5d7345a0c5c7995
luiz.arruda <luiz.arruda113@gmail.com>
2025-05-09 16:28:03 -0300

    feat(usecase): implement DeleteEventUseCase for event removal logic

---
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Application/UseCases/DeleteEventUseCase.cs
@@ -0,0 +1,38 @@
+﻿using SchedulingBetta.API.Domain.Interfaces;
+using SchedulingBetta.API.Domain.Interfaces.IEventUseCases;
+
+namespace SchedulingBetta.API.Application.UseCases;
+
+public class DeleteEventUseCase : IDeleteEventUseCase
+{
+    private readonly IEventRepository _eventRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<DeleteEventUseCase> _logger;
+
+    public DeleteEventUseCase(
+        IEventRepository eventRepository,
+        IUnitOfWork unitOfWork,
+        ILogger<DeleteEventUseCase> logger)
+    {
+        _eventRepository = eventRepository;
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+
+    public async Task<bool> Execute(int id)
+    {
+        _logger.LogInformation("Deleting event with ID {EventId}", id);
+        var eventToDelete = await _eventRepository.GetEventById(id);
+        if (eventToDelete == null)
+        {
+            _logger.LogWarning("Event with ID {EventId} not found", id);
+            return false;
+        }
+
+        await _eventRepository.DeleteEvent(eventToDelete);
+        await _unitOfWork.Commit();
+        
+        _logger.LogInformation("Event with ID {EventId} deleted successfully", id);
+        return true;
+    }
+}
