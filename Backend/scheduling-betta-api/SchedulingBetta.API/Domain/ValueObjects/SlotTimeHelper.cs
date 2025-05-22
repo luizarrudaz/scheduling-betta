3fe2ee69abe0831171ceb94bd707c02b382125bd
luiz.arruda <luiz.arruda113@gmail.com>
2025-05-22 10:22:27 -0300

    feat(schedule): add SlotTimeHelper for time slot calculations

---
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Domain/ValueObjects/SlotTimeHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace SchedulingBetta.API.Domain.ValueObjects;
+
+public record SlotTimeHelper
+{
+    public static DateTime Normalize(DateTime inputTime, int AvailableSlots)
+    {
+        var totalMinutes = inputTime.Hour * 60 + inputTime.Minute;
+        var roundedMinutes = (totalMinutes / AvailableSlots) * AvailableSlots;
+        return new DateTime(inputTime.Year, inputTime.Month, inputTime.Day, roundedMinutes / 60, roundedMinutes % 60, 0);
+    }
+}
