4fc0d06a07916993aae049860d86c03e68971d55
luiz.arruda <luiz.arruda113@gmail.com>
2025-05-29 13:46:21 -0300

    refactor(event): update IEventRepository to align with new aggregate structure

--- Backend/scheduling-betta-api/SchedulingBetta.API/Domain/Interfaces/IEventRepository.cs
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Domain/Interfaces/IEventRepository.cs
@@ -7,7 +7,7 @@ namespace SchedulingBetta.API.Domain.Interfaces;
 public interface IEventRepository
 {
     Task<List<GetEventDto>> GetAllEvents();
-    Task<EventEntity?> GetEventById(int id, CancellationToken cancellationToken = default);
+    Task<Event?> GetEventById(int id, CancellationToken cancellationToken = default);
     Task<EventEntity?> GetEventByName(string name, CancellationToken cancellationToken = default);
     Task<IEnumerable<EventEntity>> GetEventByDateRange(DateTime start, DateTime end, CancellationToken cancellationToken = default);
     Task<int> GetAvailableSlots(int eventId, CancellationToken cancellationToken = default);
@@ -19,8 +19,8 @@ public interface IEventRepository
     //Task<bool> IsUserInWaitlist(int eventId, string userId, CancellationToken cancellationToken = default);
     Task<EventEntity> AddEvent(Event @event, CancellationToken cancellationToken = default);
     Task AddEventRange(IEnumerable<EventEntity> events, CancellationToken cancellationToken = default);
-    Task UpdateEvent(EventEntity @event, CancellationToken cancellationToken = default);
-    Task DeleteEvent(EventEntity @event, CancellationToken cancellationToken = default);
+    Task UpdateEvent(Event @event, CancellationToken cancellationToken = default);
+    Task DeleteEvent(Event @event, CancellationToken cancellationToken = default);
     Task AddInterestedUser(EventSchedule schedule, CancellationToken cancellationToken = default);
     Task<EventSchedule?> GetInterestedUser(int eventId, string userId);
     Task<List<EventSchedule>> GetAllSchedules();
