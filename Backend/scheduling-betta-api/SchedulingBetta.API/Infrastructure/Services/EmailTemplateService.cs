897522649cf6507e42e086569ebc6068bece7fe6
luiz.arruda <luiz.arruda113@gmail.com>
2025-05-29 13:46:30 -0300

    feat(email): implement EmailTemplateService for handling templates

---
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Infrastructure/Services/EmailTemplateService.cs
@@ -0,0 +1,25 @@
+﻿using SchedulingBetta.API.Domain.Interfaces.ISmtp;
+
+namespace SchedulingBetta.API.Infrastructure.Services;
+
+public class EmailTemplateService : IEmailTemplateService
+{
+    private readonly string _templateDirectory = @"C:\Users\luiz.arruda\OneDrive - NOOVI DO BRASIL LTDA\Área de Trabalho\scheduling-betta\Backend\scheduling-betta-api\SchedulingBetta.API\Infrastructure\EmailTemplates\";
+
+    public string GetTemplateContent(string templateName, Dictionary<string, string> placeholders)
+    {
+        var templatePath = Path.Combine(_templateDirectory, templateName);
+        if (!File.Exists(templatePath))
+            throw new FileNotFoundException($"Template {templateName} não encontrado em {_templateDirectory}");
+
+        var content = File.ReadAllText(templatePath);
+
+        foreach (var placeholder in placeholders)
+        {
+            content = content.Replace($"{{{{{placeholder.Key}}}}}", placeholder.Value);
+        }
+
+        return content;
+    }
+}
+
