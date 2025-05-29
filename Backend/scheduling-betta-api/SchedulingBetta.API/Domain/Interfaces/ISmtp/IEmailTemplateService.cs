f9339879c0e051bd1e889be3f39a0cefd0aa0ff7
luiz.arruda <luiz.arruda113@gmail.com>
2025-05-29 13:46:29 -0300

    feat(email): define ISmtp interfaces for abstraction

---
+++ Backend/scheduling-betta-api/SchedulingBetta.API/Domain/Interfaces/ISmtp/IEmailTemplateService.cs
@@ -0,0 +1,6 @@
+﻿namespace SchedulingBetta.API.Domain.Interfaces.ISmtp;
+
+public interface IEmailTemplateService
+{
+    string GetTemplateContent(string templateName, Dictionary<string, string> placeholders);
+}
